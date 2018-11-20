160fd8da6aa0ccabbc3f04f429e664116d4d0a19
Frederick Chessell <ChessellFC@cardiff.ac.uk>
2018-11-20 14:16:39 +0000

    Updated example to work with new library

--- examples/SimpleTest.ino
+++ examples/SimpleTest.ino
@@ -1,19 +1,24 @@
-#include "IOTLib.h" // Requires WebSocket library 1.X.X to be installed
+#include "IOTLib.h"
+
+// Enter a MAC address for your controller below.
+// Newer Ethernet shields have a MAC address printed on a sticker on the shield
+const byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+// Create library instance with server info
+// IP, port, Arduino Mac address, Arduino ethernet shield pin number
+IOTLib iotLib("192.168.0.1", 81, mac, 10);
+
+
 
 const int dialPin = A0;
-const int sendInterval = 1000;
 int lastDialReading = 0;
 
 // Setup code here is ran once (before loop is called):
-void setup() {
-  
-}
+void setup() { }
 
 // Code here runs repeatedly:
 void loop() {
-	IOTLib.loop("192.168.0.1", 81, "arduino", "trial"); // Maintain socket.io connection
+	iotLib.sendDialReading("button1", analogRead(dialPin)); // Send server the new update
 
-	IOTLib.sendDialReading("button1", analogRead(potPin)); // Send server the new update
-
-	delay(sendInterval); // Prevent the server being flooded with updates
+	delay(1000); // Prevent the server being flooded with updates
 }
