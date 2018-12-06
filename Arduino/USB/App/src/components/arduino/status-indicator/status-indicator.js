70197e35b49c3c9f4311b85ce27cf345f98fc943
Jonathan Cole <JonathanPeterCole@gmail.com>
2018-12-06 16:08:15 +0000

    Added status indicator images

--- Arduino/USB/App/src/components/arduino/status-indicator/status-indicator.js
+++ Arduino/USB/App/src/components/arduino/status-indicator/status-indicator.js
@@ -3,12 +3,27 @@ import PropTypes from 'prop-types'
 
 import Spinner from '../../spinner/spinner'
 
+import ConnectedIcon from './img/connected.svg'
+import DisconnectedIcon from './img/disconnected.svg'
+import ErrorIcon from './img/error.svg'
+
+import './style.css'
+
 export default class StatusIndicator extends React.Component {
   render () {
     return (
       <div className='status-indicator'>
         { this.props.status === 'connecting' &&
           <Spinner />
+        }
+        { this.props.status === 'connected' &&
+          <img src={ConnectedIcon} />
+        }
+        { this.props.status === 'disconnected' &&
+          <img src={DisconnectedIcon} />
+        }
+        { this.props.status === 'error' &&
+          <img src={ErrorIcon} />
         }
       </div>
     )
