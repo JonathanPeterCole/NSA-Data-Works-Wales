44a7fe4708cc28bb81dfaeb3f8120cece7943089
Jonathan Cole <JonathanPeterCole@gmail.com>
2018-12-09 14:16:39 +0000

    Network status improvements

--- Arduino/USB/App/src/components/network-status/network-status.js
+++ Arduino/USB/App/src/components/network-status/network-status.js
@@ -7,39 +7,17 @@ import './style.css'
 import ConnectedIcon from './img/connected.svg'
 
 export default class NetworkStatus extends React.Component {
-  constructor (props) {
-    super(props)
-    // Bindings
-    this.getClassName = this.getClassName.bind(this)
-  }
-
-  getClassName () {
-    if (this.props.connected && this.props.show) {
-      return 'network-status show online'
-    } else if (!this.props.connected && this.props.show) {
-      return 'network-status show offline'
-    } else if (this.props.connected && !this.props.show) {
-      return 'network-status online'
-    } else {
-      return 'network-status offline'
-    }
-  }
-
   render () {
     return (
-      <div className={this.getClassName()}>
-        {this.props.connected &&
-          <div className='status'>
-            <img className='status-icon' src={ConnectedIcon} />
-            <div className='status-message'>Connected to Data Works</div>
-          </div>
-        }
-        {!this.props.connected &&
-          <div className='status'>
-            <Spinner className='status-icon' />
-            <div className='status-message'>Attempting to connect...</div>
-          </div>
-        }
+      <div className={this.props.show ? 'network-status show' : 'network-status'}>
+        <div className='status offline'>
+          <Spinner className='status-icon' />
+          <div className='status-message'>Attempting to connect...</div>
+        </div>
+        <div className={this.props.connected ? 'status online show' : 'status online'}>
+          <img className='status-icon' src={ConnectedIcon} />
+          <div className='status-message'>Connected to Data Works</div>
+        </div>
       </div>
     )
   }
